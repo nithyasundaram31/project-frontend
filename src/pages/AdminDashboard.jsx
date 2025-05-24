b794b59f4c638eaab1198427754141738a9e4bd5
nithya <nithukitty112@gmail.com>
2025-05-24 15:07:27 +0530

    footer changes

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,4 +1,4 @@
-import Footer from "../components/Footer";
+// import Footer from "../components/Footer";
 import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
