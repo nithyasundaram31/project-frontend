fb24a69fbe1793667116f459909bda6d45a4a33e
nithya <nithukitty112@gmail.com>
2025-06-14 21:03:05 +0530

    backend url

--- src/services/instance.js
+++ src/services/instance.js
@@ -11,9 +11,9 @@ instance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
     if (token) config.headers.Authorization = `Bearer ${token}`;
-    return config;
+    return config;  
   },
   (error) => Promise.reject(error)
 );
 
-export default instance;
+export default instance; 
