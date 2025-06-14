137a8aa826d34c7c486ffa756221b511f4aec762
nithya <nithukitty112@gmail.com>
2025-06-14 14:08:10 +0530

    change login,resiter,admin colrs

--- src/pages/AdminLogin.jsx
+++ src/pages/AdminLogin.jsx
@@ -21,9 +21,12 @@ const AdminLogin = () => {
   };
 
   return (
-    <div className='flex justify-center items-center h-screen bg-gradient-to-tr from-orange-400 to-blue-300'>
-      <form onSubmit={handleLogin} className='bg-white p-8 rounded shadow-lg w-96'>
-        <h2 className='text-2xl mb-6 text-center'>Admin Login</h2>
+    <div className='mx-auto items-center h-screen bg-gradient-to-tr from-blue-600  to-yellow-200'>
+       <div className='p-4 '>
+                <h2 className="text-4xl mt-8 font-bold text-center text-blue-900">Online Assessment Platform</h2>
+            </div>
+      <form onSubmit={handleLogin} className='bg-white mx-auto mt-10 p-8 rounded shadow-lg w-96'>
+        <h2 className='text-3xl mb-6  font-bold text-center'>Admin Login</h2>
         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}
           placeholder="Email" className='w-full p-2 mb-4 border rounded' />
         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}
